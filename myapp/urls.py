3a17ace5d36b4dd221ca67f44fa9900700ee4a74
h_mandakini <himasha.mandakini@gmail.com>
2021-05-29 00:58:52 +0530

    CRUD operations

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.TaskView.as_view({'get': 'list'}), name="tasks")
+    path("", views.TaskView.as_view({'get': 'list'}), name="tasks"),
+    path("get/<str:id>", views.getTaskByID, name="getTasks"),
+    path("add/", views.createTask, name="createtask"),
+    path("edit/<str:id>", views.updateTask, name="edittask"),
+    path("delete/<str:id>", views.deleteTask, name="deltask"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,40 @@ from .models import Task
 from rest_framework import viewsets
 from .serializers import TaskSerializer
 
+from django.http import JsonResponse, HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 # Create your views here.
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+@api_view(['GET'])
+def getTaskByID(request, id):
+    getTask = Task.objects.filter(id= id)
+    serializer = TaskSerializer(getTask, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def createTask(request):
+    newTask = TaskSerializer(data=request.data)
+    if newTask.is_valid():
+        newTask.save()
+    return Response(newTask.data)
+
+@api_view(['PUT'])
+def updateTask(request, id):
+    taskObj = Task.objects.get(id= id)
+    updatedTaskObj = TaskSerializer(instance=taskObj, partial=True, data=request.data)
+    if updatedTaskObj.is_valid(raise_exception=True):
+        updatedTaskObj.save()
+        updated_data = updatedTaskObj.data
+        return Response(updated_data)
+    return Response(updatedTaskObj.data)
+
+@api_view(['DELETE'])
+def deleteTask(request, id):
+    taskObj = Task.objects.get(id= id)
+    taskObj.delete()
+    return Response("Task Deleted Successfully")
